258916328279835e9b23e997f156a3b6ac992957
oreoluwade <oreoluwade@gmail.com>
2021-10-27 07:15:15 +0100

    Add DB connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { config } from "dotenv";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BookModule } from "./book/book.module";
 
+config();
 @Module({
-  imports: [BookModule],
+  imports: [
+    BookModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gntz4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
